fe403d1ed229e30aaabe9ac0131378ed91091387
gammafn <GammaFunction@vivaldi.net>
2020-02-01 02:17:25 -0600

    just add fpath

--- toggle-command-prefix.zsh
+++ toggle-command-prefix.zsh
@@ -2,8 +2,7 @@
 # ref: zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
-[[ $zsh_loaded_plugins[-1] != */*toggle-command-prefix && -z $fpath[(r)${0:h}] ]] &&
-	fpath+=("${0:h}/functions")
+fpath+=("${0:h}/functions")
 autoload -Uz .toggle-command-prefix split-shell-arguments
 # }}}
 
