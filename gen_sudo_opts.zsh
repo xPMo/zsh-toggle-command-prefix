ee77cf3bda91b364ec53a637caf218da4fce7fd8
gammafn <GammaFunction@vivaldi.net>
2020-02-01 01:26:38 -0600

    Add CUTBUFFER, fix a few flags, use 'end_loc'
    gen options before publish

---
+++ gen_sudo_opts.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+emulate -L zsh
+setopt extendedglob
+
+autoload _sudo
+
+_arguments(){ _sudo_args=(${@:2}) }
+_sudo 2>/dev/null
+_sudo_args=(${_sudo_args:#*_command_names*})
+sudo_args_gnu=(${${${(M)_sudo_args:#*\]::*}#*\)}%%(|-)\[*})
+sudo_args=(${${${(M)_sudo_args:#*\]:[^:]*}#*\)}%%\[*})
+sudo_flags=(${${${_sudo_args:#*\]:*}#*\)}%%\[*})
+set -- 'sudo ' 'sudo' \
+	"(${(j:|:)sudo_flags}|(${(j:|:)${(@)sudo_args/%+/?*}}|${(j:|:)sudo_args_gnu})*)" \
+	"(${(j:|:)${(@)sudo_args%[=+]}})*"
+
+print -r - "${(qq)@}"
